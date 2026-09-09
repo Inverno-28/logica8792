3415486ce20d8589bfe10a25693ee2f68bc679ef
inverno-28 <daniel.winter2807@gmail.com>
2026-09-09 20:35:59 -0300

    calculadora

--- main.c
+++ main.c
@@ -2,10 +2,22 @@
 
 int main(){
 
-    int a = 5;
-    int b = 4;
+    int a;
+    int b;
 
-    printf("O maior numero é: %d\n", (a> b) ? a : b);
+    printf("digite a: ");
+        scanf ("%d",&a);
+  
+    printf("digite a: ");
+        scanf ("%d",&b);
 
+    printf("A soma dos numero é: %d\n", (a + b));
+
+    printf("A subtrção dos numero é: %d\n", (a - b));
+    
+    printf("A multiplicação dos numeros é: %d\n", (a * b));
+
+    printf("A divisão dos numeros é: %.2f\n", (float)a / b);
+    
     return 0;
 }
