9d4b91a98cf163863831ab4e73f07d8f9702e297
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-11 20:30:46 -0300

    cotação

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<locale.h>
+
+int main(){
+
+setlocale (LC_ALL, "pt_BR.UTF-8");
+
+float valor, cotação;
+
+printf("digite o valor em reais: ");
+scanf("%f",&valor);
+
+printf("digite o valor em dolar: ");
+scanf("%f", &cotação);
+
+printf("valor em dolares: %2f\n", valor / cotação);
+
+    return 0;
+}
