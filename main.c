b0f6f97da4481052aca8432f621741270e4b3fd7
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-11 21:14:25 -0300

    media

--- main.c
+++ main.c
@@ -5,15 +5,14 @@ int main(){
 
 setlocale (LC_ALL, "pt_BR.UTF-8");
 
-float raio;
-const float PI = 3.14159;
+ float n1, n2, n3, media;
 
-printf("digite o raio do circulo ");
-scanf("%f", &raio);
+    printf("digite 3 notas" );
+        scanf("%f %f %f", &n1, &n2, &n3);
 
-printf("area: %.2f\n", PI * raio * raio);
-printf("perimetro %.2f\n", 2 * PI * raio);
+    media = (n1 + n2 + n3) / 3;
+
+    printf("media: %.2f\n", media);
 
     return 0;
-    //so para comitar
 }
