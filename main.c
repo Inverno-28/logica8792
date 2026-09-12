be6b73d4ff66a49763253bd18e73894222bbf1d0
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-11 21:28:56 -0300

    celsios para farenhaeithtiy

--- main.c
+++ main.c
@@ -3,16 +3,13 @@
 
 int main(){
 
-setlocale (LC_ALL, "pt_BR.UTF-8");
+float celsius, fahrenheit;
 
- float n1, n2, n3, media;
+printf("digite a temperatura em celsius: ");
+scanf("%f", &celsius);
 
-    printf("digite 3 notas" );
-        scanf("%f %f %f", &n1, &n2, &n3);
-
-    media = (n1 + n2 + n3) / 3;
-
-    printf("media: %.2f\n", media);
-
+fahrenheit = (celsius * 9.0 / 5.0) + 32;
+printf("fahrenheit: %.2f\n", fahrenheit);
+    
     return 0;
 }
