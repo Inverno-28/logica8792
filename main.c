56eef616c4914ed550ed79669d6f540405de6d13
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-18 20:54:47 -0300

    consante vogal

--- main.c
+++ main.c
@@ -6,48 +6,18 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int m;
+char l;
 
-    printf("digite um numero respequitivo ao mes do ano: ");
-    scanf("%d", &m);
+printf("digite uma letra: ");
+scanf("%c", &l);
 
-    switch (m)
+if (l == 'a' || l == 'e' || l == 'i' || l == 'o' || l == 'u' || 
+    l == 'A' || l == 'E' || l == 'I' || l == 'O' || l == 'A'){
+        printf("vogal\n");
+    }else{
+        printf("consoante\n");
+}
 
-    {
-
-    case 12: 
-    case 1: 
-    case 2:
-        printf("verão");
-        break;
-
-    case 3:
-    case 4:
-    case 5:
-        printf("outono");
-        break;
-
-    case 6:
-    case 7:
-    case 8: 
-        printf("inverno");
-        break;
-    
-    case 9:
-    case 10:
-    case 11:
-        printf("primavera");
-        break;
-  
-    //case 13:
-        //printf("todos os anos deveria ter 13 meses, eles teriam tds 4 semanas e exataos 28 dias cada, e tds as semanas começariam e terminariam com domingo e sabado");
-        //break;
-        
-    default:
-        printf("valor invalido");
-        break;
-        
-    }
 
     return 0;
 }
