0b36879d3347b881c161ab96de27dd6b3886ee08
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-18 20:11:05 -0300

    estações com base no mes exer1

--- main.c
+++ main.c
@@ -6,35 +6,48 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int opcao;
+int m;
 
-printf("--- SISTEMA DE PEDAGIO ---\n");
-printf("1 - moto\n");
-printf("2 - carro passeio\n");
-printf("3 - caminhao\n");
-printf("4 - onibus\n");
-scanf("%d", &opcao);
+    printf("digite um numero respequitivo ao mes do ano: ");
+    scanf("%d", &m);
 
-switch (opcao)
-{
-case 1:
-    printf("categotia: Moto | tarida R$ 5,00\n");
-    break;
-case 2:
-    printf("Categoria: carro passeio | tarifa R$ 10,00\n");
-    break;
-case 3:
-    printf("Categoria: caminhao | tarifa: R$ 25,00\n");
-    break;
-case 4:
-    printf("Categoria: onibus | tarifa:R$ 30,00\n");
-    break;
-default:
-    printf("erro opcao invalida");
-    break;
+    switch (m)
 
-}
+    {
 
+    case 12: 
+    case 1: 
+    case 2:
+        printf("verão");
+        break;
+
+    case 3:
+    case 4:
+    case 5:
+        printf("outono");
+        break;
+
+    case 6:
+    case 7:
+    case 8: 
+        printf("inverno");
+        break;
+    
+    case 9:
+    case 10:
+    case 11:
+        printf("primavera");
+        break;
+  
+    //case 13:
+        //printf("todos os anos deveria ter 13 meses, eles teriam tds 4 semanas e exataos 28 dias cada, e tds as semanas começariam e terminariam com domingo e sabado");
+        //break;
+        
+    default:
+        printf("valor invalido");
+        break;
+        
+    }
 
     return 0;
 }
