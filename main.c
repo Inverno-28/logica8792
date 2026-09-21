e420456399b4a05af47490b3cab36b564659aa8c
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-21 19:40:22 -0300

    bomba com for

--- main.c
+++ main.c
@@ -6,33 +6,11 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-float peso, altura, imc;
-
-printf("digite seu peso (KG): ");
-scanf("%f", &peso);
-
-printf("digite sau altura (m): ");
-scanf("%f", &altura);
-
-imc = peso / (altura * altura);
-
-if(imc<18.5){
-    printf("seu peso é; %.2f\n", peso);
-    printf("sua altura é; %.2f\n", altura);
-    printf("classificação; abaixo do peso!\n");
-}else if (imc<25){
-    printf("seu peso é; %.2f\n", peso);
-    printf("sua altura é; %.2f\n", altura);
-    printf("peso normal");
-}else if (imc<30){
-    printf("seu peso é; %.2f\n", peso);
-    printf("sua altura é; %.2f\n", altura);
-    printf("sobre peso");
-}else{
-    printf("seu peso é; %.2f\n", peso);
-    printf("sua altura é; %.2f\n", altura);
-    printf("obseidade");
+for(int i = 1; i <= 10000; i++){
+    printf("%d\n", i);
 }
+
+    printf("boooooommm");
 
 return 0;
 
