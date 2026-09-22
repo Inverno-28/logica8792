1fdb8e6c4d2d3903097bd0db1b12b30043fcf06a
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-22 20:56:29 -0300

    aaaaaaaaaaaa

--- main.c
+++ main.c
@@ -13,15 +13,14 @@ int multiplicar(int a, int b){
     return a * b;
 }
 
-int dividr(int a, int b){
+int dividir(int a, int b){
     if(b <= 0){
-        printf("ele precisa ser maior que 0")
+        printf("ele precisa ser maior que 0");
     }else{
     return a + b;
     }
   
 }
-
 
 // void soma (){
 //     int num1, num2;
@@ -35,11 +34,16 @@ int dividr(int a, int b){
 
 int main() {
 
-    SetConsoleCP(65001);
-    SetConsoleOutputCP(65001);
+SetConsoleCP(65001);
+SetConsoleOutputCP(65001);
 
-int resultado = somar(5,3);
-printf("o resultado da soma é %d", resultado);
-
+int r = somar(5,3);
+int r1 = subtract(10,2);
+int r2 = multiplicar(4,2);
+int r3 = dividir(16,2);
+printf("resultado soma %d\n ", r);
+printf("resultado subtração %d\n ", r1);
+printf("resultado multiplicação %d\n ", r2);
+printf("resultado divisão %d\n ", r3);
     return 0;
 }
