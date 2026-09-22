2f6b455b26609a465bfd36fe228de74e2a712f70
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-22 20:06:11 -0300

    Função eu definitivamente n sei o q to fazendo

--- main.c
+++ main.c
@@ -5,11 +5,23 @@ int somar(int a, int b){
     return a + b;
 }
 
+// void soma (){
+//     int num1, num2;
+//     printf("digite um valor para num1");
+//     scanf("%d", &num1);
+//     printf("digite um valor para num2");
+//     scanf("%d", &num2);
+//     int resultado1 = num1 + num2;
+//     printf ("resultado com void é %d\n", resultado1);
+// }
+
 int main() {
 
-    int resultado = somar (5, 3);
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 
-    printf("resultado da soma é %d", resultado);
-   
+int resultado = somar(5,3);
+printf("o resultado da soma é %d", resultado);
+
     return 0;
 }
