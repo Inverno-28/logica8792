4e9bb4b0f3df0e19b7504847fd7ae9394538b7d8
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-21 21:12:06 -0300

    0+2

--- main.c
+++ main.c
@@ -6,23 +6,10 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int numerosInteiros[5] = {1, 5, 10, 15, 20};
-float numerosFloats[5] = {10.5, 20.8, 30.2, 40.9, 50.1};
-char guitarra[3][20] = {"Les Paul", "StratoCaster", "Fly V"};
+for(int i = 0; i <= 10; i = i + 2){
+    printf("%d\n", i);
+}
 
-for (int i = 0; i < 5; i++){
-    printf ("%d\n", numerosInteiros[i]);}
-
-printf("\n");
-
-for (int j = 0; j < 5; j++){
-    printf ("%.2f\n", numerosFloats[j]);}
-
-printf("\n");
-
-for (int k = 0; k < 3; k++){
-    printf ("%s\n", guitarra[k]);}
- 
 return 0;
 
 }
