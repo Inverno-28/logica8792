a247c4a41131677565e8ac33300a17897190859e
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-21 21:29:57 -0300

    for com soma

--- main.c
+++ main.c
@@ -6,9 +6,20 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-for(int i = 10; i >= 0; i = i - 2){
-    printf("%d\n", i);
+int soma = 0;
+int i;
+
+for(int i = 1; i <= 5; i++){
+    soma = soma + i;
+    //soma + i = resultado
+    //0 + 1 = 1
+    //1 + 2 = 3
+    //3 + 3 = 6
+    //6 + 4 = 10
+    //10 + 5 = 15
 }
+
+printf("soma é %d", soma);
 
 return 0;
 
