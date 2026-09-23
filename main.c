8cbc50dfa6dde5d8018fb1a2a62a499db8b22fef
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-23 20:40:19 -0300

    Tabuada Pedindo Valor

--- main.c
+++ main.c
@@ -3,19 +3,33 @@
 #include <math.h>
 #include <string.h>
 
-void tabuada(int n){
-    printf("tabuada do %d\n", n);
-    for(int i = 1; i <= 10; i++){
-        printf("%d x %d = %d\n", n, i, n * i);
+// void tabuada(int n){
+//     printf("tabuada do %d\n", n);
+//     for(int i = 1; i <= 10; i++){
+//         printf("%d x %d = %d\n", n, i, n * i);
+//     }
+// }
+
+int tabuada(int n){
+    int resultado;
+    printf("tabuada do %d:\n", n);
+    for (int i = 1; i <= 10; i++){
+        resultado = n * i;
+        printf("%d x %d = %d\n", n, i, resultado);
     }
+    
+    return resultado;
+
 }
 
 int main() {
-
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-tabuada(8);
+int numero;
+    printf("digite um numero para ver a tabuada: ");
+    scanf("%d",&numero);
+    tabuada(numero);
 
     return 0;
 }
