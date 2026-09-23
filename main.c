67608edb89fed8df2c509a3310d332a37f68352b
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-23 20:58:31 -0300

    Par ou Impar

--- main.c
+++ main.c
@@ -3,33 +3,20 @@
 #include <math.h>
 #include <string.h>
 
-// void tabuada(int n){
-//     printf("tabuada do %d\n", n);
-//     for(int i = 1; i <= 10; i++){
-//         printf("%d x %d = %d\n", n, i, n * i);
-//     }
-// }
-
-int tabuada(int n){
-    int resultado;
-    printf("tabuada do %d:\n", n);
-    for (int i = 1; i <= 10; i++){
-        resultado = n * i;
-        printf("%d x %d = %d\n", n, i, resultado);
+void verificarEhParImpar(int n){
+    if (n % 2 == 0){
+        printf("%d é par\n",n);
+    }else{
+        printf("%d é impar\n", n);
     }
-    
-    return resultado;
-
 }
 
-int main() {
+int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int numero;
-    printf("digite um numero para ver a tabuada: ");
-    scanf("%d",&numero);
-    tabuada(numero);
+verificarEhParImpar(10);
+verificarEhParImpar(7);
 
     return 0;
 }
