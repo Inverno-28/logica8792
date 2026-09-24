b670c6d8ea3a6bec262edc3b65a6e2cefd82a997
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-24 20:12:02 -0300

    ATM

--- main.c
+++ main.c
@@ -7,18 +7,29 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int num1;
+int opcao = -1;
 
-printf("digite um numero positivo: ");
-    scanf("%d", &num1);
-
-    while(num1 <= 0){
+    while(opcao != 0){
     
-        printf("Numero Invalálido! digite novamente: ");
-            scanf("%d", &num1);
-}
+        printf("\nMenu: \n");
+        printf("1 - depositar\n");
+        printf("2 - sacar\n");
+        printf("3 - consultar\n");
+        printf("0 - sairo\n");
+        printf("Escolha: \n"); 
+            scanf("%d", &opcao);
 
-printf("Numero valido: %d\n", num1);
+    switch (opcao){
+        
+            case 1: printf("Voce escolheu depositar\n");break;
+            case 2: printf("Voce escolheu sacar\n");break;
+            case 3: printf("Voce escolheu consultar\n");break;
+            case 0: printf("Saindo\n");break;
+        
+        default: printf("Opção invalida\n");
+        }
+
+    }
 
     return 0;
 }
