209fa277c7d4c6f5082a86cca87bfdde25096dee
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-25 20:10:55 -0300

    Numeros Perfeitos

--- main.c
+++ main.c
@@ -1,32 +1,32 @@
+
 #include <stdio.h>
 #include <windows.h>
 #include <math.h>
 #include <string.h>
-
-int ehPrimo(int n){
-    if(n < 2) return 0; 
-    for (int i = 2; i < n; i++){
-        if(n % i == 0) return 0;
-    }
-    return 1;
-
-}
 
 int main(){
 
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int numero;
-    
-printf("Digite um numero: ");
-    scanf("%d", &numero);
-   
-    if(ehPrimo(numero))
-        printf("%d é primo\n", numero);
-    else
-        printf("%d não é primo\n", numero);
+int n, soma = 0;
 
-return 0;
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i < n; i++){
+        if(n % i == 0){
+            soma += i;
+        }
+    }
+
+    if (soma == n){
+        printf("%d é um numero perfeito\n", n);
+    }else{
+        printf("%d não é um numero perfeito\n", n);
+    }
+    
+
+return 0; 
 
 }
