741361f906000104e3fab329af6f1676ec33c80b
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-25 19:40:23 -0300

    Primos

--- main.c
+++ main.c
@@ -8,17 +8,29 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-int numero, contador = 0;
+int n, primo = 1;
 
-printf("digite numeros (0 para parar): \n");
-scanf("%d", &numero);
+    printf("digite um numero: ");
+        scanf("%d",&n);
 
-while (numero != 0){
-    contador++;
-    scanf("%d", &numero);
+if(n < 2){ // aqui caso o numero seja menor q 2 (tds os negaticos, 0 e 1) ele ja vai falar direto q n é primo
+    primo = 0;
+}else{
+    for(int i = 2; i <= n / 2; i++){ // aqui ele vai checar se o numero é divisivel e se sobra resto, se n sobrar é primo
+        if (n % i == 0){
+            primo = 0;
+            break;
+        }   
+    }
 }
 
-printf("quantidade de numeros digitados: %d\n", contador);
+//aqui ele vai fazer uma verificação de 0 ou 1 encima da variavel primo para ser verdadeira ou falsa
+if (primo){
+    printf("%d é primo", n);
+}else{
+    printf("%d não é primo", n);
+}
+
 
 return 0;
 
