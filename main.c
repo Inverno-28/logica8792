197c0158158b51a787ddebcbe5fa316bcbfa9f05
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-25 21:43:43 -0300

    Calculadora 2000

--- main.c
+++ main.c
@@ -7,17 +7,59 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-   int n;
-   float nota, soma = 0;
+int opcao;
+float a, b;
 
-    printf("quantos alunos tem a turma: ");
-    scanf("%d",&n);
-    for (int i = 0; i < n; i++){
-        printf("digite a nota do aluno %d: \n", i + 1);
-        scanf("%f", &nota);
-        soma += nota;
+do{
+    printf("\nMENU");
+    printf("\n1 soma");
+    printf("\n2 subtração");
+    printf("\n3 multi");
+    printf("\n4 divisao");
+    printf("\n0 sair");
+    printf("escolha um opção: ");
+        scanf("%d", &opcao);
+
+    switch (opcao){
+        case 1:
+            printf("digite 2 numeros");
+            scanf("%f %f", &a,&b);
+            printf("resultado: %.2f\n", a + b);
+            break;
+        
+        case 2:
+            printf("digite 2 numeros");
+            scanf("%f %f", &a,&b);
+            printf("resultado: %.2f\n", a - b);
+            break;    
+
+        case 3:
+            printf("digite 2 numeros");
+            scanf("%f %f", &a,&b);
+            printf("resultado: %.2f\n", a * b);
+            break;
+
+        case 4:
+            printf("digite 2 numeros");
+            scanf("%f %f", &a,&b);
+            if(b != 0)
+                printf("resultado: %.2f\n", a / b);
+            else
+                printf("erro tonto divisão por zero");
+            break;
+
+            case 0:
+                printf("saindo");
+                break;
+    
+        default: 
+            printf("n sei");
+            break;
     }
-    printf("media da turma: %2.f\n", soma/n);
+}while (opcao != 0);{
+    
+}
+
 
 return 0; 
 
