3a7510131209f6962e00d2e9e4d452590dfafafb
Inverno-28 <daniel.winter2807@gmail.com>
2026-09-25 21:24:54 -0300

    Media De Notas 3000

--- main.c
+++ main.c
@@ -7,18 +7,17 @@ int main(){
 SetConsoleCP(65001);
 SetConsoleOutputCP(65001);
 
-   int n, resto, invertido = 0;
+   int n;
+   float nota, soma = 0;
 
-   printf("digite um valor: ");
-    scanf("%d", &n);
-
-    while(n != 0){
-        resto = n % 10; // 512 % 10 = 2
-        invertido = invertido * 10 + resto;
-        n /= 10; // (n + n)/10
+    printf("quantos alunos tem a turma: ");
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++){
+        printf("digite a nota do aluno %d: \n", i + 1);
+        scanf("%f", &nota);
+        soma += nota;
     }
-   
-    printf("numero invertido: %d\n", invertido);
+    printf("media da turma: %2.f\n", soma/n);
 
 return 0; 
 
